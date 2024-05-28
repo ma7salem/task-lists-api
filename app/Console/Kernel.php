c932300481cb5501c62563e137d4ad87ffaeb2c5
ma7moudsalem1 <ma7moudsalem1@gmail.com>
2024-05-28 09:51:19 +0300

    changing time zone in schedule to egypt cairo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('tasks:start')->daily();
+         $schedule->command('tasks:start')
+                  ->daily()
+                  ->timezone('Africa/Cairo');
     }
 
     /**
